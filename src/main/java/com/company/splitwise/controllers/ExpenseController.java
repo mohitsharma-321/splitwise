d2e81283dc35c757d38eb2a6e306ee30d2b7ad70
Mohit Sharma <chiragmohitsharma@gmail.com>
2026-04-16 23:46:29 +0530

    creating expense functionality now

--- src/main/java/com/company/splitwise/controllers/ExpenseController.java
+++ src/main/java/com/company/splitwise/controllers/ExpenseController.java
@@ -1,4 +1,20 @@
 package com.company.splitwise.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class ExpenseController {
+
+    @PostMapping("/expense")
+    public void createExpense() {
+
+    }
+
+    @GetMapping("/expense/:{expenseId}")
+    public void getExpense() {
+
+    }
 }
