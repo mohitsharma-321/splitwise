8e4068112ca1998c1bb7740782ecc059c2f983eb
Mohit Sharma <chiragmohitsharma@gmail.com>
2026-04-27 23:00:17 +0530

    Added GroupController Structure and Endpoints

---
+++ src/main/java/com/company/splitwise/controllers/GroupController.java
@@ -0,0 +1,69 @@
+package com.company.splitwise.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class GroupController {
+
+    @PostMapping("/group")
+    public void createGroup(@RequestBody Object createGroupRequest){
+
+    }
+
+    @GetMapping("/group/{groupId}")
+    public void getGroup(@PathVariable Long groupId) {
+
+    }
+
+    @PostMapping("/group/{groupId}/settle")
+    public void settleGroupExpenses(@PathVariable Long groupId) {
+
+    }
+
+}
+
+
+
+//
+//
+//package com.scaler.splitwise.controllers;
+//
+//import com.scaler.splitwise.dtos.CreateGroupDTO;
+//import com.scaler.splitwise.dtos.GroupDTO;
+//import com.scaler.splitwise.dtos.ResponseDTO;
+//import com.scaler.splitwise.dtos.SettleResponseDTO;
+//import com.scaler.splitwise.models.Group;
+//import com.scaler.splitwise.services.GroupService;
+//import com.scaler.splitwise.utils.ResponseUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+//import java.util.Optional;
+//
+//public class GroupController {
+//    @Autowired
+//    private GroupService groupService;
+//
+//    @PostMapping("/group")
+//    public ResponseDTO<GroupDTO> createGroup(@RequestBody CreateGroupDTO createGroupRequest) {
+//        GroupDTO group = groupService.createGroup(createGroupRequest);
+//        return ResponseUtils.toResponse(Optional.of(group));
+//    }
+//
+//    @GetMapping("/group/{groupId}")
+//    public ResponseDTO<GroupDTO> getGroup(@PathVariable Long groupId) {
+//        Optional<Group> group = groupService.getGroup(groupId);
+//        return ResponseUtils.toResponse(group.map(GroupDTO::from));
+//    }
+//
+//    @PostMapping("/group/{groupId}/settle")
+//    public ResponseDTO<SettleResponseDTO> settleGroupExpenses(@PathVariable Long groupId) {
+//        Optional<SettleResponseDTO> transactions = groupService.settleExpenses(groupId);
+//        return ResponseUtils.toResponse(transactions);
+//    }
+//}
